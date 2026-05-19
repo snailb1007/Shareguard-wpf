d521d102eec228ca9494ca550aee6664bf5893b1
ethan <thiennv@cyberk.io>
2026-05-20 04:57:55 +0700

    test: add Domain and Application test projects with xUnit v3

---
+++ ShareGuard.Domain.Tests/ArchitectureTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace ShareGuard.Domain.Tests;
+
+public class ArchitectureTests
+{
+    [Fact]
+    public void Domain_ShouldNotReference_ApplicationOrInfrastructure()
+    {
+        var domainAssembly = typeof(ArchitectureTests).Assembly;
+        var referencedAssemblies = domainAssembly.GetReferencedAssemblies()
+            .Select(a => a.Name)
+            .ToList();
+
+        Assert.DoesNotContain("ShareGuard.Application", referencedAssemblies);
+        Assert.DoesNotContain("ShareGuard.Infrastructure", referencedAssemblies);
+        Assert.DoesNotContain("ShareGuard.App", referencedAssemblies);
+    }
+}
